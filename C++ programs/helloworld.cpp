16d984a31f9e13d553b41c2a790b3b0d0b2e584e
Sayedoo19 <alawisayed792@gmail.com>
2022-11-26 16:32:31 +0300

    Variables and basic data types

---
+++ C++ programs/helloworld.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main(){
+
+    // declaring variables
+    int age = 21;
+    int year = 2023;
+    int days = 7;
+
+
+    double price = 10.99;
+    double gpa = 2.5;
+    double temp = 25.1;
+
+    // single character
+    char grade = 'A';
+    char initial = 'B';
+    char dollarSign = '$';
+
+    // boolean (true or false)
+    bool student = true;
+    bool power = false;
+    bool forSale = true;
+
+    // string (objects that represent text)
+    std::string name = "John";
+    std::string day = "Monday";
+    std::string month = "December";
+
+
+    std::cout << "Hello" << name << '\n'; 
+
+    std::cout << name;
+    
+    return 0;
+}
